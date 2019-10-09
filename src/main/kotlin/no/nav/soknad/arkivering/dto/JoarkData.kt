7ba253784ab0db117d97e5f04349a7d69b667db9
Johan Sjöblom <johan.sjoblom@squeed.com>
2019-10-09 17:28:53 +0200

    Extending Archiver service

---
+++ src/main/kotlin/no/nav/soknad/arkivering/dto/JoarkData.kt
@@ -0,0 +1,3 @@
+package no.nav.soknad.arkivering.dto
+
+data class JoarkData(val id: String, val message: String, val attachedFiles: List<ByteArray>)
--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/JoarkArchiver.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/JoarkArchiver.kt
@@ -1,6 +1,7 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.service
 
 import no.nav.soknad.arkivering.dto.ArchivalData
+import no.nav.soknad.arkivering.dto.JoarkData
 import no.nav.soknad.arkivering.soknadsarkiverer.config.ApplicationProperties
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -12,12 +13,21 @@ class JoarkArchiver(private val restTemplate: RestTemplate,
 	private val logger = LoggerFactory.getLogger(javaClass)
 
 	fun archive(archivalData: ArchivalData) {
-		putDataInJoark(archivalData)
+		val attachedFiles = getFilesFromFileStorage(archivalData)
+		val joarkData = createJoarkData(archivalData, attachedFiles)
+		putDataInJoark(joarkData)
 	}
 
-	private fun putDataInJoark(archivalData: ArchivalData) {
-		logger.info("Sending to Joark: '$archivalData'")
+	private fun getFilesFromFileStorage(archivalData: ArchivalData): List<ByteArray> {
+		// TODO: fetch from file storage
+		return listOf(archivalData.message.toByteArray()) // Just return something in the mean time
+	}
+
+	private fun createJoarkData(archivalData: ArchivalData, attachedFiles: List<ByteArray>) = JoarkData(archivalData.id, archivalData.message, attachedFiles)
+
+	private fun putDataInJoark(joarkData: JoarkData) {
+		logger.info("Sending to Joark: '$joarkData'")
 		val url = applicationProperties.joarkHost + applicationProperties.joarkUrl
-		restTemplate.postForObject(url, archivalData, String::class.java)
+		restTemplate.postForObject(url, joarkData, String::class.java)
 	}
 }
