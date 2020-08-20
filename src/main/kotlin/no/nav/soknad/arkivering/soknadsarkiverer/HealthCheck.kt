fe894b62304bc478df9a2564628ba92ff8a3b592
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-08-20 11:08:40 +0200

    Adding Health endpoints

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/HealthCheck.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/HealthCheck.kt
@@ -1,4 +1,36 @@
 package no.nav.soknad.arkivering.soknadsarkiverer
 
-class HealthCheck {
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/internal"])
+class IsAlive {
+	private val logger = LoggerFactory.getLogger(javaClass)
+	private var oppstart = 0
+
+	@GetMapping("/isAlive")
+	fun isAlive(): String {
+		if (oppstart < 10) {
+			logger.info("isAlive called")
+			oppstart++
+		}
+		return "Ok"
+	}
+
+	@GetMapping("/ping")
+	fun ping(): String {
+		return "pong"
+	}
+
+	@GetMapping("/isReady")
+	fun isReady(): String {
+		if (oppstart < 10) {
+			logger.debug("isReady called")
+			oppstart++
+		}
+		return "Ready for actions"
+	}
 }
