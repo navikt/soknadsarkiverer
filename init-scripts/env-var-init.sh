b67ae24877b573262efdfcf14bbe70a68bb8260b
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-09-01 21:19:00 +0200

    SD-1108 - Setting av properties. Eksperimentering med setting av environment parametere for serviceuser-navn og -passord. Feil i init-script

--- init-scripts/env-var-init.sh
+++ init-scripts/env-var-init.sh
@@ -5,7 +5,7 @@ export CLIENTID="No authentication available"
 PATH_CLIENTID=/var/run/secrets/nais.io/serviceuser/username
 PATH_CLIENTSECRET=/var/run/secrets/nais.io/serviceuser/password
 
-if test -f "PATH_CLIENTSECRET"; then
+if test -f "$PATH_CLIENTSECRET"; then
   CLIENTID=$(cat "$PATH_CLIENTID")
   CLIENTSECRET=$(cat "$PATH_CLIENTSECRET")
   echo Exporting authentication
