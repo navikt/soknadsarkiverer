3a11492c75e4d57037403a3a946c5cf9ede5c833
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-09-04 17:03:02 +0200

    SD-1108 - Kall for å opprette Journalpost  feiler på brevkode. Tillate at melding kan være null i response

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/arkivservice/api/OpprettJournalpostResponse.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/arkivservice/api/OpprettJournalpostResponse.kt
@@ -1,7 +1,11 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.arkivservice.api
 
-data class OpprettJournalpostResponse(val dokumenter: List<Dokumenter>, val journalpostId: String,
-																			val journalpostferdigstilt: Boolean, val journalstatus: String, val melding: String)
+data class OpprettJournalpostResponse(
+	val dokumenter: List<Dokumenter>,
+	val journalpostId: String,
+	val journalpostferdigstilt: Boolean,
+	val journalstatus: String,
+	val melding: String? = null)
 
 data class Dokumenter(
 	val brevkode: String? = null,
