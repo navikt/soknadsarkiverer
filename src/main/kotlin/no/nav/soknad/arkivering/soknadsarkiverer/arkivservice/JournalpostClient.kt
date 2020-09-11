8ec956d0bf72f10f265454a9c7d601cfb7cea721
Johan Sjöblom <johan.sjoblom@squeed.com>
2020-09-11 12:06:41 +0200

    Improved code in JournalpostClient

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/arkivservice/JournalpostClient.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/arkivservice/JournalpostClient.kt
@@ -10,8 +10,12 @@ import no.nav.soknad.arkivering.soknadsarkiverer.dto.FilElementDto
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
+@Service
 class JournalpostClient(private val appConfiguration: AppConfiguration,
 												@Qualifier("archiveRestTemplate") private val restTemplate: RestTemplate): JournalpostClientInterface {
 
@@ -21,23 +25,29 @@ class JournalpostClient(private val appConfiguration: AppConfiguration,
 		return restTemplate.getForObject("${appConfiguration.config.joarkUrl}/ping", String::class.java)!!
 	}
 
-	override fun opprettjournalpost(key: String, applicationMessage: Soknadarkivschema, attachedFiles: List<FilElementDto>): String {
-		logger.info("${key}: Skal opprette journalpost for søknad med behandlingsId=${applicationMessage.getBehandlingsid()}")
-		val opprettJounalpostRequest: OpprettJournalpostRequest = createOpprettJournalpostRequest(applicationMessage, attachedFiles)
+	override fun opprettjournalpost(key: String, soknadarkivschema: Soknadarkivschema, attachedFiles: List<FilElementDto>): String {
+		try {
+			logger.info("$key: Creating journalpost with behandlingsId=${soknadarkivschema.getBehandlingsid()}")
+			val request: OpprettJournalpostRequest = createOpprettJournalpostRequest(soknadarkivschema, attachedFiles)
 
-		val url = appConfiguration.config.joarkHost + appConfiguration.config.joarkUrl
-		logger.info("${key}: joark url=${url}")
+			val url = appConfiguration.config.joarkHost + appConfiguration.config.joarkUrl
 
-		val responseEntity = restTemplate.postForEntity(url, HttpEntity<Any>(opprettJounalpostRequest), OpprettJournalpostResponse::class.java)
+			val response = sendDataToJoark(request, url)
+			val journalpostId = response?.journalpostId ?: "-1"
 
-		if (responseEntity.body != null) {
-			val journalpostId = responseEntity.body?.journalpostId ?: "-1"
-			logger.info("${key}: Opprettet journalpost med id=${journalpostId} for søknad med behandlingsId=${applicationMessage.getBehandlingsid()}.")
+			logger.info("$key: Saved to Joark, got the following journalpostId: '$journalpostId'")
 			return journalpostId
-		}
-		throw ArchivingException(RuntimeException("${key}: Feil ved forsøk på å opprette journalpost for ${applicationMessage.getBehandlingsid()}." +
-			" Response status = ${responseEntity.statusCodeValue}"))
 
+		} catch (e: Exception) {
+			logger.error("$key: Error sending to Joark", e)
+			throw ArchivingException(e)
+		}
 	}
 
+	private fun sendDataToJoark(data: OpprettJournalpostRequest, url: String): OpprettJournalpostResponse? {
+		val headers = HttpHeaders()
+		headers.contentType = MediaType.APPLICATION_JSON
+		val request = HttpEntity(data, headers)
+		return restTemplate.postForObject(url, request, OpprettJournalpostResponse::class.java)
+	}
 }
