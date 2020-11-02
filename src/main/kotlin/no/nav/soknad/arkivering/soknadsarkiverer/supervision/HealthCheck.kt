e9c9c2ab7b1db9724ae4bcc7e470d12086389819
Johan Sjöblom <johan.sjoblom@nav.no>
2020-11-02 18:44:08 +0100

    Adding swagger to admin service

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/supervision/HealthCheck.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/supervision/HealthCheck.kt
@@ -1,5 +1,6 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.supervision
 
+import io.swagger.v3.oas.annotations.Hidden
 import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping(value = ["/internal"])
 class HealthCheck {
 
+	@Hidden
 	@GetMapping("/isAlive")
 	@Unprotected
 	fun isAlive() = "Ok"
 
+	@Hidden
 	@GetMapping("/ping")
 	@Unprotected
 	fun ping() = "pong"
 
+	@Hidden
 	@GetMapping("/isReady")
 	@Unprotected
 	fun isReady() = "Ready for actions"
