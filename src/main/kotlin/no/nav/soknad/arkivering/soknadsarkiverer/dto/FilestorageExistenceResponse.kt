2b2ba47fc0fa47da05cd0669360947e4d600bea5
Johan Sjöblom <johan.sjoblom@nav.no>
2020-11-02 18:44:08 +0100

    Adding swagger to admin service

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/dto/FilestorageExistenceResponse.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/dto/FilestorageExistenceResponse.kt
@@ -1,3 +1,15 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.dto
 
-data class FilestorageExistenceResponse(val id: String, val status: String)
+import io.swagger.v3.oas.annotations.media.Schema
+import javax.validation.constraints.NotBlank
+
+data class FilestorageExistenceResponse(
+	@Schema(description = "The key of a file in Filestorage",
+		example = "22d47115-215e-4e0f-a5c8-3dc38d837159", required = true)
+	@NotBlank
+	val id: String,
+
+	@Schema(description = "Whether the file exists or not.", //TODO: Introduce enum?
+		example = "Does not exist", required = true)
+	@NotBlank
+	val status: String)
