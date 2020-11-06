dc882fa6d9555724e12d57b2070357dd839e86a7
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-11-06 10:21:25 +0100

    SD-1233 - Beskytte admin rest endepunktene med basic authentication. Deployer ikke dersom man fjerner EnableJwtTokenValidation annotering

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/config/JwtTokenValidationConfig.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/config/JwtTokenValidationConfig.kt
@@ -6,12 +6,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @EnableOAuth2Client(cacheEnabled = true)
-/*@EnableJwtTokenValidation(ignore = [
+@EnableJwtTokenValidation(ignore = [
 	"org.springframework",
 	"no.nav.soknad.arkivering.soknadsarkiverer.admin.AdminInterface",
 	"no.nav.soknad.arkivering.soknadsarkiverer.supervision.HealthCheck"])
-*/
 @Profile("dev | prod")
 @Configuration
-//class SecurityConfiguration
 class JwtTokenValidationConfig
