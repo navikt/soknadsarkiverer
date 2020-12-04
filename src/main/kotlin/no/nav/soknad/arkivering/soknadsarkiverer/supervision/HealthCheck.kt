642dc0bbbcad84fbd0890b8c721cc754aa26586b
nils-arne <50109859+nils-arne@users.noreply.github.com>
2020-12-04 12:31:29 +0100

    SD-1267- Lagt inn ping kall mot soknadsfillager og joark for å kunne teste integrasjon

--- src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/supervision/HealthCheck.kt
+++ src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/supervision/HealthCheck.kt
@@ -34,7 +34,7 @@ class HealthCheck(private val appConfiguration: AppConfiguration,
 		val fileServicePong = fileService.ping()
 		val joarkServicePong = joarkService.ping()
 		logger.info("Ping called: fileServicePong=${fileServicePong}, joarkServicePong=${joarkServicePong}")
-		return if (fileServicePong.equals("pong", true) && joarkServicePong.equals("Application", true)) {
+		return if (fileServicePong.equals("pong", true) && joarkServicePong.contains("is alive",true)) {
 			"pong"
 		} else {
 			"down"
