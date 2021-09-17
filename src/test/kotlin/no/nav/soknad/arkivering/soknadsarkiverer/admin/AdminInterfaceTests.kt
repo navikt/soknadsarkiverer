36e5d0792cce195c657d5f38832ff5bf76a34df9
Johan Sjöblom <johan.sjoblom@nav.no>
2021-09-17 11:16:59 +0200

    Fixing warning

--- src/test/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/admin/AdminInterfaceTests.kt
+++ src/test/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/admin/AdminInterfaceTests.kt
@@ -22,8 +22,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
