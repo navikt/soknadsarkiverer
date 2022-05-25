8be57ff584ffdd5f305d492a4213f8a1c7609c4b
Alexander Petrov <alexander.petrov@nav.no>
2022-05-25 12:03:36 +0200

    do some echo

--- init-scripts/env-var-init.sh
+++ init-scripts/env-var-init.sh
@@ -3,6 +3,10 @@
 export CLIENTID="No authentication available"
 export CLIENTSECRET="No authentication available"
 
+echo "KAFKA_SCHEMA_REGISTRY= $KAFKA_SCHEMA_REGISTRY"
+echo "$KAFKA_SCHEMA_REGISTRY_USER = $KAFKA_SCHEMA_REGISTRY_USER"
+echo "KAFKA_SCHEMA_REGISTRY_PASSWORD= $KAFKA_SCHEMA_REGISTRY_PASSWORD"
+
 PATH_CLIENTID=/var/run/secrets/nais.io/serviceuser/username
 PATH_CLIENTSECRET=/var/run/secrets/nais.io/serviceuser/password
 
