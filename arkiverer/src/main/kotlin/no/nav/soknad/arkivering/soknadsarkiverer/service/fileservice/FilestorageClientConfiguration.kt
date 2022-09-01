de68e6aa85f7ebc871d77628b4e68e8ae819c749
Johan Sjöblom <johan.sjoblom@nav.no>
2022-09-01 17:01:55 +0200

    Minor cleanup

--- arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/fileservice/FilestorageClientConfiguration.kt
+++ arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/fileservice/FilestorageClientConfiguration.kt
@@ -1,6 +1,5 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.service.fileservice
 
-import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.soknad.arkivering.soknadsarkiverer.service.tokensupport.TokenService
@@ -8,7 +7,6 @@ import okhttp3.OkHttpClient
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 
 @Configuration
@@ -17,7 +15,11 @@ class FilestorageClientConfiguration {
 	@Bean
 	@Profile("prod | dev")
 	@Qualifier("filestorageClient")
-	fun filestorageClient(clientConfigProperties: ClientConfigurationProperties, oAuth2AccessTokenService: OAuth2AccessTokenService): OkHttpClient {
+	fun filestorageClient(
+		clientConfigProperties: ClientConfigurationProperties,
+		oAuth2AccessTokenService: OAuth2AccessTokenService
+	): OkHttpClient {
+
 		val clientProperties = clientConfigProperties.registration["soknadsfillager"]
 		val tokenService = TokenService(clientProperties!!, oAuth2AccessTokenService)
 
@@ -25,7 +27,7 @@ class FilestorageClientConfiguration {
 			val token = tokenService.getToken()
 
 			val bearerRequest = it.request().newBuilder().headers(it.request().headers)
-				.header("Authorization", "Bearer ${token.accessToken}").build()
+				.header("Authorization", "Bearer $token").build()
 
 			it.proceed(bearerRequest)
 		}.build()
--- arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/tokensupport/TokenService.kt
+++ arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/service/tokensupport/TokenService.kt
@@ -3,6 +3,6 @@ package no.nav.soknad.arkivering.soknadsarkiverer.service.tokensupport
 import no.nav.security.token.support.client.core.ClientProperties
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 
-class TokenService(val clientProperties: ClientProperties, val oauth2TokenService: OAuth2AccessTokenService) {
-	fun getToken() = oauth2TokenService.getAccessToken(clientProperties)
+class TokenService(private val clientProperties: ClientProperties, private val oauth2TokenService: OAuth2AccessTokenService) {
+	fun getToken(): String = oauth2TokenService.getAccessToken(clientProperties).accessToken
 }
