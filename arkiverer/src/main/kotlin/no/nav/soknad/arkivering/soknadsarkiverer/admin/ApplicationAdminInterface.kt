a7dad6d0806f61930929384c264ad220fbe9f71d
nilsarne <nils-arne.ringstad@nav.no>
2023-10-10 13:28:00 +0200

    Endringer etter PR. I tillegg lagt til endring for konfigurering av properties for soknadsarkiverer ved kkjøring av loadtester.

--- arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/admin/ApplicationAdminInterface.kt
+++ arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/admin/ApplicationAdminInterface.kt
@@ -1,22 +1,16 @@
 package no.nav.soknad.arkivering.soknadsarkiverer.admin
 
-import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.soknad.arkivering.api.AdminApi
 import no.nav.soknad.arkivering.model.ArchivingStatus
 import no.nav.soknad.arkivering.soknadsarkiverer.service.TaskListService
-import no.nav.soknad.arkivering.soknadsarkiverer.service.safservice.SafService
 import no.nav.soknad.arkivering.soknadsarkiverer.service.safservice.SafServiceInterface
+import no.nav.soknad.arkivering.soknadsarkiverer.util.konverterTilDateTime
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import java.time.LocalDateTime
-import java.time.OffsetDateTime
-import java.time.format.DateTimeFormatter
 
 @RestController
 @ProtectedWithClaims(issuer = "azuread")
@@ -51,12 +45,5 @@ class ApplicationAdminInterface(private val taskListService: TaskListService, pr
 
 	}
 
-	private fun konverterTilDateTime(dateString: String): OffsetDateTime {
-		if (dateString.isBlank()) return OffsetDateTime.MIN
-		val formatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME
-		val dateTime = LocalDateTime.parse(dateString, formatter)
-		val zoneOffSet = OffsetDateTime.now().offset
-		return dateTime.atOffset(zoneOffSet)
-	}
 
 }
