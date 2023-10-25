c05ff64070a464c739598f907811c11cc540939a
nilsarne <nils-arne.ringstad@nav.no>
2023-10-25 08:52:09 +0200

    Lagt til test på om leader før trigging av restart. Lagt til re-skedulering. Fjernet ubrukt import

--- arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/config/SelfDestructConfig.kt
+++ arkiverer/src/main/kotlin/no/nav/soknad/arkivering/soknadsarkiverer/config/SelfDestructConfig.kt
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import java.time.Instant
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.ZoneId
 
 @Configuration
